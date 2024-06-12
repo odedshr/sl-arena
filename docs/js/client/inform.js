2d87d1c5677f89d1d576c31cf669519e56c97c23
Oded Sharon <odedshr@Odeds-MacBook-Pro.local>
2024-06-12 16:39:16 +0100

    🐛 Fixed dark console text

--- docs/js/client/inform.js
+++ docs/js/client/inform.js
@@ -1,4 +1,4 @@
-const CONSOLE_STYLE = `font-size:1rem;color:darkblue; font-family: 'MyCustomFont', 'Courier New', Courier, monospace`;
+const CONSOLE_STYLE = `font-size:1rem;background-color:white;color:darkblue; font-family: 'MyCustomFont', 'Courier New', Courier, monospace`;
 function inform(...elements) {
     console.log(`%c${elements[0]}`, CONSOLE_STYLE, ...elements.slice(1));
 }
